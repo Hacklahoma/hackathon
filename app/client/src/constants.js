ec6b92cfa6736ddaeb5b34ce9d25ece789aa78a9
s-fletcher <sfletchertx@gmail.com>
2020-01-20 19:53:53 -0600

    fixed confirmation message

--- app/client/src/constants.js
+++ app/client/src/constants.js
@@ -12,10 +12,10 @@ angular.module('reg')
         SUBMITTED: 'Feel free to edit it at any time. However, once registration is closed, you will not be able to edit it any further.\nPlease make sure your information is accurate before registration is closed!',
         CLOSED_AND_INCOMPLETE_TITLE: 'Unfortunately, registration has closed. We hope to see you next year!',
         CLOSED_AND_INCOMPLETE: 'If you are an OU student and still want to attend, we will accept walk-ins.',
-        ADMITTED_AND_CAN_CONFIRM_TITLE: 'You must confirm by [CONFIRM_DEADLINE])',
-        ADMITTED_AND_CANNOT_CONFIRM_TITLE: 'Your confirmation deadline of [CONFIRM_DEADLINE]) has passed.',
+        ADMITTED_AND_CAN_CONFIRM_TITLE: 'You must confirm by February 1st, 2020 at 11:59 PM CST',
+        ADMITTED_AND_CANNOT_CONFIRM_TITLE: 'Your confirmation deadline has passed.',
         ADMITTED_AND_CANNOT_CONFIRM: 'Although you were accepted, you did not complete your confirmation in time.\nUnfortunately, this means that you will not be able to attend the event, as we must begin to accept other applicants on the waitlist.\nWe hope to see you again next year!',
-        CONFIRMED_NOT_PAST_TITLE: 'You can edit your confirmation information until [CONFIRM_DEADLINE])',
+        CONFIRMED_NOT_PAST_TITLE: 'You can edit your confirmation information until February 1st, 2020 at 11:59 PM CST',
         DECLINED: 'We\'re sorry to hear that you won\'t be able to make it to Hacklahoma 2020! :(\nMaybe next year! We hope you see you again soon.',
     })
     .constant('TEAM',{
